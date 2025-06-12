5916a8f991134577a941412be547b01dd0047298
DebangshuKundu-VerilogCode <debangshurnjkundu@gmail.com>
2025-06-12 16:28:28 +0530

    Add files via upload

---
+++ BASIC GATES/basic_gates_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: basic_gates_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module basic_gates_tb();
+reg a,b;
+wire y0,y1,y2,y3,y4,y5,y6;
+
+basic_gates DUT(a,b,y0,y1,y2,y3,y4,y5,y6);
+
+initial
+begin
+    a=1'b0; b=1'b0;
+#10 a=1'b0; b=1'b1;
+#10 a=1'b1; b=1'b0;
+#10 a=1'b1; b=1'b1;
+#10 $finish;
+end
+endmodule
