1f7062927480a1d3cb5e0a86d1affede85bc0025
DebangshuKundu-VerilogCode <debangshurnjkundu@gmail.com>
2025-06-12 22:01:51 +0530

    Add files via upload

---
+++ MUX 16 to 1/MUX_16to1_TB.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: MUX_16to1_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX_16to1_TB();
+reg [15:0] in;
+reg [3:0] sel;
+wire f;
+
+MUX_16to1 DUT (in,sel,f);
+
+initial
+begin
+sel = 4'b0000; in = 16'haa56;
+#5 sel = 4'b0101;
+#5 sel = 4'b0111;
+#5 sel = 4'b0001;
+#5 $finish;
+end
+
+initial
+$monitor ($time, "sel = %b, in=%h, f=%b", sel,in,f);
+endmodule
