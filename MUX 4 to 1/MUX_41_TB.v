60fa5d599b2a0b691ce43eb3e931f19445443976
DebangshuKundu-VerilogCode <debangshurnjkundu@gmail.com>
2025-06-12 21:44:20 +0530

    Add files via upload

---
+++ MUX 4 to 1/MUX_41_TB.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: MUX_41_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX_41_TB();
+reg [3:0] i;
+reg [1:0] sel;
+wire f;
+
+MUX_41_bv DUT (i,sel,f);
+
+initial 
+begin
+  /*
+sel=2'b00; i=4'b1010;
+$display ("sel =%b, f=%b",sel,f);
+
+#10 sel=2'b01; i=4'b1010;
+$display ("sel =%b, f=%b",sel,f);
+
+#10 sel=2'b10; i=4'b1010;
+$display ("sel =%b, f=%b",sel,f);
+
+#10 sel=2'b11; i=4'b1010;
+$display ("sel =%b, f=%b",sel,f);
+#10 $finish;
+end
+*/
+i = 4'd3;
+i = 4'd4;
+i = 4'd6;
+i = 4'd8;
+
+   sel = 2'b00;
+#5 sel = 2'b01;
+#5 sel = 2'b10;
+#5 sel = 2'b11;
+#5;
+#5 i = 4'd4;
+#5 i = 4'd2;
+#5 i = 4'd7;
+#5 i = 4'd6;
+end
+initial #40 $finish;
+
+
+endmodule
---
+++ MUX 4 to 1/MUX_41_bv.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: MUX_41_bv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// BEHAVIORAL MODELLING
+module MUX_41_bv(
+input [3:0] i,
+input [1:0] sel,
+output reg f    );
+
+always@(*)
+begin
+casex(sel)
+    2'b00 : f = i[0];
+    2'b01 : f = i[1];
+    2'b10 : f = i[2];
+    2'b11 : f = i[3];
+    default : f = 1'bx;
+endcase
+end
+    //       'OR'
+/*
+always@(*)
+begin
+    f = sel[1] ? (sel[0] ? i[3] : i[2]) : (sel[0] ? i[1] : i[1]);
+end
+*/
+endmodule
