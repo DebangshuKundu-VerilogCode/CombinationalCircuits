84047ed709fdef10e5dfb53a8201995e5c4bccbd
DebangshuKundu-VerilogCode <debangshurnjkundu@gmail.com>
2025-06-25 19:11:27 +0530

    Add files via upload

---
+++ BCD SUBTRACTOR/bcd_subtractor.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: bcd_subtractor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bcd_subtractor(
+input [3:0] x,y,
+input cin,
+output [3:0] w,
+output sign,
+output [4:0] ans
+    );
+
+wire [3:0] p;    
+compliment_10 cc (
+    .b(y),
+    .c(cin),
+    .carry(),
+    .s(p)
+    );
+
+wire [3:0] t;
+wire o;
+BCD_adder ba (
+    .a(x),
+    .b(p),
+    .cin(1'b0),
+    .sum(t),
+    .cout(o)
+    );  
+      
+compliment_10 cc0 (
+    .b(t),
+    .c(~o),
+    .carry(),
+    .s(w)
+    );
+assign sign = ~o;    
+assign ans = {sign,w};
+endmodule
---
+++ BCD SUBTRACTOR/bcd_subtractor_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: bcd_subtractor_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bcd_subtractor_tb();
+reg [3:0] x,y;
+reg cin;
+wire sign;
+wire [3:0] w;
+wire [4:0] ans;
+
+bcd_subtractor DUT (
+    .x(x),
+    .y(y),
+    .cin(cin),
+    .sign(sign),
+    .ans(ans),
+    .w(w)
+    );
+    
+ initial 
+    #200 $finish ;
+    
+initial
+begin
+         cin = 1'b1; x = 4'd4; y = 4'd7;
+    #100 x = 4'd3; y = 4'd8;    
+end
+
+endmodule
